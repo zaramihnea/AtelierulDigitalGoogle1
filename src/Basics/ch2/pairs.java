c31c78b09d88b449139195e7ffe9ccb21f11f8fa
Zara Mihnea <zaramihnea@icloud.com>
2022-11-16 19:43:45 +0200

    a

---
+++ src/Basics/ch2/pairs.java
@@ -0,0 +1,23 @@
+package Basics.ch2;
+
+import java.util.Objects;
+
+public class pairs {
+    private int value1, value2;
+    public pairs(int value1, int value2) {
+        this.value1 = value1;
+        this.value2 = value2;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        pairs pairs = (pairs) o;
+        return value1 == pairs.value1 && value2 == pairs.value2;
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(value1, value2);
+    }
+
+}
