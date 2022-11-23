f88a380eef96a3a0dcc619fa21b4e69d0755d336
Zara Mihnea <zaramihnea@icloud.com>
2022-11-23 20:44:15 +0200

    ok

---
+++ src/Basics/curs4/Animal.java
@@ -0,0 +1,29 @@
+package Basics.curs4;
+//create a superclass named animal and three subclasses named Dog, Cat and Fish
+
+public class Animal {
+    protected Integer legs;
+}
+
+
+//crate a Pet interface specified by the UML diagram
+//create a class named Dog that implements the Pet interface
+//create a class named Cat that implements the Pet interface
+//create a class named Fish that implements the Pet interface
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/Basics/curs4/first.java
+++
@@ -1,5 +0,0 @@
-package Basics.curs4;
-
-public class first {
-
-}
